efdb363c4fe1ae3fb917e71bc3ae19d0e6d0236c
Angeschossen <!FlashLight2018>
2020-06-06 10:18:20 +0200

    Removed unused code

--- src/main/java/me/angeschossen/lands/api/land/enums/SortMode.java
+++ src/main/java/me/angeschossen/lands/api/land/enums/SortMode.java
@@ -1,19 +1,9 @@
 package me.angeschossen.lands.api.land.enums;
 
-import org.jetbrains.annotations.NotNull;
 
 public enum SortMode {
 
-    SIZE, BALANCE, MEMBERS;
-
-    @NotNull
-    public static SortMode getByName(String name) {
-        for (SortMode sortMode : values()) {
-            if (sortMode.toString().equalsIgnoreCase(name)) {
-                return sortMode;
-            }
-        }
-
-        return SortMode.BALANCE;
-    }
+    SIZE,
+    BALANCE,
+    MEMBERS
 }
