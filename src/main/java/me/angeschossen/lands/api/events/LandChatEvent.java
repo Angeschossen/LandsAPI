01c816bea0d68a86e111d5f31b313f2c937a9a89
Angeschossen <!FlashLight2018>
2020-07-23 22:04:58 +0200

    Chat event

--- src/main/java/me/angeschossen/lands/api/events/LandChatEvent.java
+++ src/main/java/me/angeschossen/lands/api/events/LandChatEvent.java
@@ -1,6 +1,7 @@
 package me.angeschossen.lands.api.events;
 
 import me.angeschossen.lands.api.land.Land;
+import me.angeschossen.lands.api.player.LandPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
@@ -9,21 +10,22 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 
 
 public class LandChatEvent extends Event implements Cancellable {
     public static HandlerList handlerList = new HandlerList();
-    private boolean cancelled;
-
     private final String message;
     private final UUID playerUID;
     private final Land land;
-    private final Collection<UUID> recipients;
+    private final Collection<LandPlayer> recipients;
     private final MessageSource messageSource;
+    private boolean cancelled;
 
-    public LandChatEvent(Land land, UUID playerUID, Collection<UUID> recipients, String message, MessageSource messageSource) {
+    public LandChatEvent(Land land, UUID playerUID, Collection<LandPlayer> recipients, String message, MessageSource messageSource) {
         this.land = land;
         this.playerUID = playerUID;
         this.message = message;
@@ -31,20 +33,28 @@ public class LandChatEvent extends Event implements Cancellable {
         this.messageSource = messageSource;
     }
 
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @Deprecated
     public Collection<UUID> getRecipients() {
+        return Collections.emptyList();
+    }
+
+    @NotNull
+    public Collection<LandPlayer> getReceivers() {
         return recipients;
     }
 
+    @NotNull
     public Land getLand() {
         return land;
     }
 
+    @NotNull
     public MessageSource getSource() {
         return messageSource;
-    }
-
-    public enum MessageSource {
-        MINECRAFT, DISCORD
     }
 
     @NotNull
@@ -57,12 +67,9 @@ public class LandChatEvent extends Event implements Cancellable {
         return Bukkit.getPlayer(playerUID);
     }
 
+    @NotNull
     public String getMessage() {
         return message;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlerList;
     }
 
     @Override
@@ -83,5 +90,9 @@ public class LandChatEvent extends Event implements Cancellable {
     @Override
     public String toString() {
         return "Sender: " + getSenderUID().toString() + " Land: " + land.getName();
+    }
+
+    public enum MessageSource {
+        MINECRAFT, DISCORD
     }
 }
