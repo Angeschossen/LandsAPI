382c9f63fd1c5ce5cc5ebc3a5275601af5858022
Angeschossen <!FlashLight2018>
2020-08-18 20:34:24 +0200

    WarDeclareEvent

---
+++ src/main/java/me/angeschossen/lands/api/events/war/WarDeclareEvent.java
@@ -0,0 +1,57 @@
+package me.angeschossen.lands.api.events.war;
+
+import me.angeschossen.lands.api.war.WarEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class WarDeclareEvent extends Event implements Cancellable {
+
+    public static HandlerList handlerList = new HandlerList();
+    private final @NotNull
+    WarEntity attacker, defender;
+    private boolean cancelled;
+
+    public WarDeclareEvent(@NotNull WarEntity attacker, @NotNull WarEntity defender) {
+        super(!Bukkit.isPrimaryThread());
+
+        this.attacker = attacker;
+        this.defender = defender;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @NotNull
+    public WarEntity getAttacker() {
+        return attacker;
+    }
+
+    @NotNull
+    public WarEntity getDefender() {
+        return defender;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    @Override
+    public String toString() {
+        return "WarDeclareEvent{attacker=" + attacker.toString() + ",defender=" + defender.toString() + "}";
+    }
+}
