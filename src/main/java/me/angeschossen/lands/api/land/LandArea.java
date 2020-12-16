edbffab9f7f6067edbf19c35d284b63ee08c464b
Angeschossen <!FlashLight2018>
2020-12-16 20:40:40 +0100

    BoundingBox

--- src/main/java/me/angeschossen/lands/api/land/LandArea.java
+++ src/main/java/me/angeschossen/lands/api/land/LandArea.java
@@ -1,15 +1,21 @@
 package me.angeschossen.lands.api.land;
 
+import me.angeschossen.lands.api.blockworks.BoundingBox;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface LandArea extends Area {
 
-    boolean contains(int x, int y, int z);
-
     @Nullable
     World getWorld();
+
+    /**
+     * Get the bounding box of the sub area.
+     * @return Will return null if the area is not setup {@link #isSetup()}.
+     */
+    @Nullable
+    BoundingBox getBoundingBox();
 
     boolean setName(@NotNull String name);
 
