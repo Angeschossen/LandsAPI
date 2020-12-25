c204eb85c1080091b557a8c8253e333c9d50f0b4
Angeschossen <!FlashLight2018>
2020-12-25 11:13:24 +0100

    added getDeleter()

--- src/main/java/me/angeschossen/lands/api/events/LandDeleteEvent.java
+++ src/main/java/me/angeschossen/lands/api/events/LandDeleteEvent.java
@@ -2,23 +2,29 @@ package me.angeschossen.lands.api.events;
 
 import me.angeschossen.lands.api.land.Land;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class LandDeleteEvent extends Event implements Cancellable {
 
     public static HandlerList handlerList = new HandlerList();
     private final Land land;
     private final Reason reason;
+    private final @Nullable
+    CommandSender deleter;
     private boolean cancelled;
 
-    public LandDeleteEvent(Land land, Reason reason) {
+
+    public LandDeleteEvent(Land land, Reason reason, @Nullable CommandSender deleter) {
         super(!Bukkit.isPrimaryThread());
 
         this.land = land;
         this.reason = reason;
+        this.deleter = deleter;
     }
 
     public static HandlerList getHandlerList() {
@@ -43,6 +49,11 @@ public class LandDeleteEvent extends Event implements Cancellable {
     @Override
     public void setCancelled(boolean cancelled) {
         this.cancelled = cancelled;
+    }
+
+    @Nullable
+    public CommandSender getDeleter() {
+        return deleter;
     }
 
     @Override
