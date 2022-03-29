1e25c00e3b1b79fb75763072bfa663cbd3d2dbd8
David Schwalm <angeschossen2019@gmail.com>
2022-03-29 21:00:28 +0200

    getTrustedPlayers

--- src/main/java/me/angeschossen/lands/api/land/Area.java
+++ src/main/java/me/angeschossen/lands/api/land/Area.java
@@ -11,25 +11,33 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface Area {
 
+    double addTax(double tax);
+
     boolean banPlayer(UUID uuid);
 
-    void unbanPlayer(UUID playerUID);
+    boolean canEnter(@NotNull LandPlayer landPlayer, boolean sendMessage);
 
-    boolean isBanned(@NotNull UUID playerUID);
+    @NotNull
+    String getColorName();
+
+    @NotNull
+    Role getEntryRole();
+
+    @Nullable
+    Invite getInvite(@NotNull UUID receiverUUID);
+
+    Land getLand();
+
+    @NotNull
+    String getName();
 
     @NotNull
     UUID getOwnerUID();
-
-    boolean isDefault();
-
-    @Deprecated
-    boolean hasLandSetting(LandSetting naturalFlags);
-
-    boolean hasFlag(@NotNull LandFlag flag);
 
     @NotNull
     Role getRole(@NotNull UUID playerUID);
@@ -37,41 +45,36 @@ public interface Area {
     @NotNull
     Role getRole(@NotNull String name);
 
-    Land getLand();
+    double getTax();
 
-    @NotNull
-    Role getEntryRole();
+    void setTax(double rent);
+
+    @NotNull Collection<UUID> getTrustedPlayers();
 
     @NotNull
     Role getVisitorRole();
+
+    boolean hasFlag(@NotNull LandFlag flag);
+
+    boolean hasFlag(@NotNull UUID playerUUID, @NotNull RoleFlag flag);
+
+    boolean hasFlag(@NotNull Player player, @NotNull RoleFlag flag, @Nullable Material material, boolean sendMessage);
+
+    boolean hasFlag(@NotNull Player player, @NotNull RoleFlag roleFlag, boolean sendMessage);
+
+    @Deprecated
+    boolean hasLandSetting(LandSetting naturalFlags);
+
+    boolean isBanned(@NotNull UUID playerUID);
+
+    boolean isDefault();
+
+    boolean isTrusted(UUID playerUID);
 
     boolean toggleFlag(@NotNull LandFlag flag);
 
     @Deprecated
     boolean toggleLandSetting(LandSetting naturalFlags);
 
-    boolean isTrusted(UUID playerUID);
-
-    boolean hasFlag(@NotNull UUID playerUUID, @NotNull RoleFlag flag);
-
-    boolean canEnter(@NotNull LandPlayer landPlayer, boolean sendMessage);
-
-    boolean hasFlag(@NotNull Player player, @NotNull RoleFlag flag, @Nullable Material material, boolean sendMessage);
-
-    boolean hasFlag(@NotNull Player player, @NotNull RoleFlag roleFlag, boolean sendMessage);
-
-    @Nullable
-    Invite getInvite(@NotNull UUID receiverUUID);
-
-    double addTax(double tax);
-
-    double getTax();
-
-    void setTax(double rent);
-
-    @NotNull
-    String getName();
-
-    @NotNull
-    String getColorName();
+    void unbanPlayer(UUID playerUID);
 }
