e1e57d6209cb9422985621a7dc82f015ddf03086
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-12-04 21:54:43 +0100

    DeleteReason additions

--- src/main/java/me/angeschossen/lands/api/events/land/DeleteReason.java
+++ src/main/java/me/angeschossen/lands/api/events/land/DeleteReason.java
@@ -1,5 +1,12 @@
 package me.angeschossen.lands.api.events.land;
 
 public enum DeleteReason {
-    DEFAULT, UPKEEP, ADMIN, INACTIVITY, UNKNOWN, WAR, CAMP_EXPIRED, NO_CLAIMS
+    DEFAULT,
+    UPKEEP,
+    ADMIN,
+    INACTIVITY,
+    @Deprecated EXPIRED,
+    WAR_CAPTURED,
+    CAMP_EXPIRED,
+    NO_CLAIMS
 }
