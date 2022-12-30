0c0b9dcbd0705dcf7243f58b9bf27898c822e11e
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-12-30 18:17:36 +0100

    6.26.9

---
+++ src/main/java/me.angeschossen.lands.api/events/player/PlayerRandomTeleportEvent.java
@@ -0,0 +1,59 @@
+package me.angeschossen.lands.api.events.player;
+
+import com.google.common.collect.ImmutableMap;
+import me.angeschossen.lands.api.events.internal.plugin.LandsPlayerEvent;
+import me.angeschossen.lands.api.land.Area;
+import me.angeschossen.lands.api.player.LandPlayer;
+import org.bukkit.Location;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class PlayerRandomTeleportEvent extends LandsPlayerEvent implements Cancellable {
+    public static final HandlerList handlerList = new HandlerList();
+    private final @NotNull Location destination;
+    private boolean cancelled = false;
+
+    public PlayerRandomTeleportEvent(@NotNull LandPlayer landPlayer, @NotNull Location location) {
+        super(landPlayer);
+
+        this.destination = location;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @NotNull
+    public Location getDestination() {
+        return destination;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public void setExpressionVariables(ImmutableMap.@NotNull Builder<String, Object> builder) {
+        super.setExpressionVariables(builder);
+
+        builder.put("destination_world", destination.getWorld().getName())
+                .put("destination_x", destination.getBlockX())
+                .put("destination_y", destination.getBlockY())
+                .put("destination_z", destination.getBlockZ());
+    }
+}
