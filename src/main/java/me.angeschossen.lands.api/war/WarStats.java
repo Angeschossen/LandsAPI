264bd6d0ee95de21cd452e042e8b2eb2b84e7e89
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-03-27 09:17:58 +0200

    doc

--- src/main/java/me.angeschossen.lands.api/war/WarStats.java
+++ src/main/java/me.angeschossen.lands.api/war/WarStats.java
@@ -2,17 +2,52 @@ package me.angeschossen.lands.api.war;
 
 public interface WarStats {
 
+    /**
+     * Get win-lost wars ratio.
+     *
+     * @return win-lost wars ratio
+     */
     double getWLRatio();
 
+    /**
+     * Get amount of wars won.
+     *
+     * @return Amount of wars that they have won
+     */
     int getWon();
 
+    /**
+     * Get amount of wars lost.
+     *
+     * @return Amount of wars that they have lost
+     */
     int getLost();
 
+    /**
+     * Get kill-death wars ratio.
+     *
+     * @return kill-death ratio
+     */
     double getKDRatio();
 
+    /**
+     * Get amount of kills.
+     *
+     * @return Amount of kills
+     */
     int getKills();
 
+    /**
+     * Get amount of deaths.
+     *
+     * @return Amount of deaths
+     */
     int getDeaths();
 
+    /**
+     * Get amount of captures. See {@link CaptureFlag} for more information.
+     *
+     * @return Amount of captures
+     */
     int getCaptures();
 }
--- src/main/java/me.angeschossen.lands.api/war/declaration/WarDeclaration.java
+++ src/main/java/me.angeschossen.lands.api/war/declaration/WarDeclaration.java
@@ -5,13 +5,40 @@ import me.angeschossen.lands.api.war.enums.WarTeam;
 
 public interface WarDeclaration {
 
+    /**
+     * Set tribute, which the surrenderer must pay to the enemy if they surrender.
+     *
+     * @param tribute Depending on the server's economy this can be cash, items etc.
+     */
     void setTribute(double tribute);
 
+    /**
+     * Set tribute, which the surrenderer must pay to the enemy if they surrender.
+     *
+     * @return Can't be negative
+     */
     double getTribute();
 
+    /**
+     * Depending on the server's config there can be a maximum tribute.
+     *
+     * @param warTeam The team
+     * @return Can't be negative
+     */
     double getMaxTribute(WarTeam warTeam);
 
+    /**
+     * Check if the war preparation time ends soon.
+     * Depends on the server's config.
+     *
+     * @return true, if the war starts in a matter of seconds
+     */
     boolean startsSoon();
 
+    /**
+     * Forcefully start this war.
+     *
+     * @return The war
+     */
     War start();
 }
