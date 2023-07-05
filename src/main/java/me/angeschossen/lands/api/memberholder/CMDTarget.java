926717434fea92945ebed3cce9c7b5871b7ccfd2
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-07-05 21:41:13 +0200

    6.32.5

--- src/main/java/me/angeschossen/lands/api/memberholder/CMDTarget.java
+++ src/main/java/me/angeschossen/lands/api/memberholder/CMDTarget.java
@@ -3,6 +3,9 @@ package me.angeschossen.lands.api.memberholder;
 import org.jetbrains.annotations.NotNull;
 
 public interface CMDTarget {
+    static String parseCMDName(String in) {
+        return in.replace(' ', '_');
+    }
 
     @NotNull
     String getCMDName();
