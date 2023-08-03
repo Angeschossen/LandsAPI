cac18b849e599f758bc7b1f36098610bd8387bfb
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-08-03 08:57:14 +0200

    doc

--- src/main/java/me/angeschossen/lands/api/utils/PlayerUtils.java
+++ src/main/java/me/angeschossen/lands/api/utils/PlayerUtils.java
@@ -6,5 +6,10 @@ import java.util.UUID;
 
 public interface PlayerUtils {
 
+    /**
+     * Getting the name of an player. The player might be offline.
+     * @param playerUID The player
+     * @return The name of the player
+     */
     @NotNull String getName(@NotNull UUID playerUID);
 }
--- src/main/java/me/angeschossen/lands/api/utils/TrustResult.java
+++ src/main/java/me/angeschossen/lands/api/utils/TrustResult.java
@@ -1,5 +1,66 @@
 package me.angeschossen.lands.api.utils;
 
 public enum TrustResult {
-    FAILURE_SELF, FAILURE_ACCESS, FAILURE_ALREADY, FAILURE_MAX_MEMBERS, FAILURE_BANNED,FAILURE_WAR, FAILURE_OWN_LAND, FAILURE_PLAYER_MAX_LANDS, FAILURE_ALREADY_INVITED, FAILURE, SUCCESS_TRUSTED, FAILURE_OFFLINE, SUCCESS_INVITED, FAILURE_PLAYER_FLAG_RECEIVE_INVITES
+    /**
+     * A player tried to trust himself.
+     */
+    FAILURE_SELF,
+    /**
+     * A player isn't allowed to trust players.
+     */
+    FAILURE_ACCESS,
+    /**
+     * The target player is already trusted.
+     */
+    FAILURE_ALREADY,
+    /**
+     * The area has reched the maximum amount of members.
+     * This is related to the lands.members.number permission.
+     */
+    FAILURE_MAX_MEMBERS,
+    /**
+     * You can't trust an banned player.
+     */
+    FAILURE_BANNED,
+    /**
+     * During war trusting players is disabled.
+     */
+    FAILURE_WAR,
+    /**
+     * The target player has its own land and isn't allowed to be a member of a land
+     * and a owner of their own land at the same time. This is related to the "invite-owner" option in config.yml.
+     */
+    FAILURE_OWN_LAND,
+    /**
+     * The player reached its maximum amount of lands, they can be a member of. This is related to the
+     * lands.lands.number permission.
+     */
+    FAILURE_PLAYER_MAX_LANDS,
+    /**
+     * The target player is already invited.
+     */
+    FAILURE_ALREADY_INVITED,
+    /**
+     * A 3rd party plugin cancelled the trust event ({@link me.angeschossen.lands.api.events.LandTrustPlayerEvent}).
+     */
+    FAILURE_PLUGIN,
+    /**
+     * If the server doesn't use Luckperms for permissions, the target player must be online.
+     * Using Luckperms maximum lands limits can be checked for offline players as well.
+     */
+    FAILURE_OFFLINE,
+    /**
+     * The target player disabled receiving invites in their personal settings menu.
+     */
+    FAILURE_PLAYER_FLAG_RECEIVE_INVITES,
+    /**
+     * The target player was invite successfully.
+     * If invitis are enabled in config.yml.
+     */
+    SUCCESS_INVITED,
+    /**
+     * The target player was trusted successfully.
+     * If the player that trusts the target is a server admin or invites are disabled in config.yml.
+     */
+    SUCCESS_TRUSTED,
 }
