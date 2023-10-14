73eb824d6af2167ada343d2f1482cf1521dfba16
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-10-14 12:38:11 +0200

    doc

--- src/main/java/me/angeschossen/lands/api/land/enums/EditPlayerResponse.java
+++ src/main/java/me/angeschossen/lands/api/land/enums/EditPlayerResponse.java
@@ -1,5 +1,19 @@
 package me.angeschossen.lands.api.land.enums;
 
+/**
+ * Used to check if a player is allowed to edit a specific role or a player that is part of that role.
+ */
 public enum EditPlayerResponse {
-    FAILURE_TENANT, FAILURE_ROLE_PRIORITY, SUCCESS
+    /**
+     * The player is the tenant of the area.
+     */
+    FAILURE_TENANT,
+    /**
+     * The role has a higher priority.
+     */
+    FAILURE_ROLE_PRIORITY,
+    /**
+     * The player is able to edit the role or player that belongs to it.
+     */
+    SUCCESS
 }
