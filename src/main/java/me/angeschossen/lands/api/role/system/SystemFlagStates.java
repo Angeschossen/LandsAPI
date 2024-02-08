996023ce313aef1a8745b6088f3e59108175cb49
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-02-08 14:48:54 +0100

    flag states

--- src/main/java/me/angeschossen/lands/api/role/system/SystemFlagStates.java
+++ src/main/java/me/angeschossen/lands/api/role/system/SystemFlagStates.java
@@ -3,19 +3,13 @@ package me.angeschossen.lands.api.role.system;
 import com.github.angeschossen.pluginframework.api.utils.Checks;
 import me.angeschossen.lands.api.LandsIntegration;
 import me.angeschossen.lands.api.flags.type.RoleFlag;
-import me.angeschossen.lands.api.handler.APIHandler;
-import me.angeschossen.lands.api.land.Area;
-import me.angeschossen.lands.api.player.LandPlayer;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
-import java.util.UUID;
-import java.util.function.Predicate;
 
-public abstract class SystemFlagStates {
-    protected final @NotNull LandsIntegration integration;
-    protected final @NotNull Set<RoleFlag> flags;
+public final class SystemFlagStates {
+    private final @NotNull LandsIntegration integration;
+    private final @NotNull Set<RoleFlag> flags;
 
     public SystemFlagStates(@NotNull LandsIntegration landsIntegration, @NotNull Set<RoleFlag> flags) {
         this.integration = Checks.requireNonNull(landsIntegration, "integration");
@@ -23,7 +17,7 @@ public abstract class SystemFlagStates {
     }
 
     @NotNull
-    public final LandsIntegration getIntegration() {
+    public LandsIntegration getIntegration() {
         return integration;
     }
 
@@ -33,7 +27,7 @@ public abstract class SystemFlagStates {
      * @param flag the flag to check
      * @return true, if this flag is included
      */
-    public final boolean hasFlag(@NotNull RoleFlag flag) {
+    public boolean hasFlag(@NotNull RoleFlag flag) {
         return flags.contains(Checks.requireNonNull(flag, "flag"));
     }
 }
