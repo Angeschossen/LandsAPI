505613f3e30ea3ccc44e4efd4273146da7555315
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-05-04 21:33:02 +0200

    roles menu

--- src/main/java/me/angeschossen/lands/api/role/enums/RoleType.java
+++ src/main/java/me/angeschossen/lands/api/role/enums/RoleType.java
@@ -7,36 +7,36 @@ import java.util.Map;
 
 public enum RoleType {
     /**
+     * The role of this type is assigned to the owner of the land or area.
+     */
+    OWNER(4, false, false, true, false, false),
+    /**
      * The role of this type is assigned for members of lands or nations that are allied
      * to a land.
      */
-    ALLY(5, false, false, false),
-    /**
-     * The role of this type is assigned to the owner of the land or area.
-     */
-    OWNER(4, false, false, true),
+    ALLY(5, false, false, false, false, true),
     /**
      * The role of this type is automatically assigned to members of the same nation that
      * aren't part of the land itself.
      */
-    NATION(3, false, false, false),
+    NATION(3, false, false, false, false, true),
     /**
      * This can be a user created role or another default role.
      */
-    NORMAL(2, true, true, true),
+    NORMAL(2, true, true, true, true, true),
     /**
      * This role is assigned to the tenant of an area.
      */
-    TENANT(6, false, false, true),
+    TENANT(6, false, false, true, false, true),
     /**
      * The role of this type is assigned to newly trusted players.
      */
-    ENTRY(1, false, false, true),
+    ENTRY(1, false, false, true, true, true),
     /**
      * The role of this type is assigned to players that aren't part of
      * any other role types.
      */
-    VISITOR(0, false, false, false);
+    VISITOR(0, false, false, false, false, false);
 
     private static final Map<Integer, RoleType> map = new HashMap<>();
 
@@ -50,16 +50,26 @@ public enum RoleType {
         }
     }
 
-    private final boolean multiple;
+    private final boolean multiple, canBeSet, canPriorityBeChanged;
     private final int id;
     private final boolean canHaveMembers;
     private boolean isDeleteable;
 
-    RoleType(int id, boolean multiple, boolean isDeleteable, boolean canHaveMembers) {
+    RoleType(int id, boolean multiple, boolean isDeleteable, boolean canHaveMembers, boolean canBeSet, boolean canPriorityBeChanged) {
         this.id = id;
         this.multiple = multiple;
+        this.canPriorityBeChanged = canPriorityBeChanged;
+        this.canBeSet = canBeSet;
         this.isDeleteable = isDeleteable;
         this.canHaveMembers = canHaveMembers;
+    }
+
+    public boolean canPriorityBeChanged() {
+        return canPriorityBeChanged;
+    }
+
+    public boolean canBeSet() {
+        return canBeSet && canHaveMembers();
     }
 
     @NotNull
