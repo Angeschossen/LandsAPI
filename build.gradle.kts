bc0e28f80f5ee68a564327a079ca8c81dc488e58
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-05-12 18:14:33 +0200

    capture flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ tasks {
 
 dependencies {
     shadow("com.github.Angeschossen:PluginFrameworkAPI:1.0.23")
-    shadow("com.github.Angeschossen:ApplicationFrameworkAPI:1.0.1")
+    shadow("com.github.Angeschossen:ApplicationFrameworkAPI:1.0.2")
     compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("org.realityforge.org.jetbrains.annotations:org.jetbrains.annotations:1.7.0")
 }
